a97da5e961293c401697d26667ee4715a5e56669
Steven Raj AL Selathurai <steven.s@u.com.my>
2024-07-23 12:19:13 +0800

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+import com.jcraft.jsch.*;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, JSchException, SftpException {
+        final JSch jsch = new JSch();
+        Session session = null;
+        ChannelSftp channelSftp = null;
+        try{
+            session = jsch.getSession("xx", "xx");
+            session.setPassword("xx");
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+            com.jcraft.jsch.Channel channel = session.openChannel("sftp");
+            channel.connect();
+            channelSftp = (ChannelSftp) channel;
+            try{
+                channelSftp.put("xx","xx");
+                System.out.println(channelSftp.ls("xx"));
+            }finally {
+                channelSftp.disconnect();
+            }
+        } finally {
+            session.disconnect();
+        }
+    }
+}
